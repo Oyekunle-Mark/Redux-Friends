ca0af6876a4c33c8679f9c770ed06173324884aa
oyekunle-mark <oyekunlemac@gmail.com>
2019-06-20 17:25:37 +0100

    Styled the header

--- friends/src/components/Header.js
+++ friends/src/components/Header.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledHeader = styled.header`
+  display: flex;
+  align-items: center;
+
+  width: 100%;
+  padding: 15px 20px;
+  background: rgb(5, 5, 5);
+
+  img {
+    width: 40px;
+    height: 40px;
+  }
+
+  h1 {
+    font-family: 'Kaushan Script', cursive;
+    font-size: 35px;
+    margin-left: 15px;
+    color: rgb(245, 30, 30);
+  }
+`;
 
 const Header = () => (
-  <header>
+  <StyledHeader>
+    <img
+      src="https://image.flaticon.com/icons/svg/1049/1049867.svg"
+      alt="friends"
+    />
     <h1>Friend Zone</h1>
-  </header>
+  </StyledHeader>
 );
 
 export default Header;
