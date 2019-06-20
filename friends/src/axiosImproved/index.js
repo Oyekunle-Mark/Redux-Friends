8538394c3f31f8247d505e6a7dc4b5532887028b
oyekunle-mark <oyekunlemac@gmail.com>
2019-06-20 15:51:31 +0100

    Created the axios request to add headers to requests

---
+++ friends/src/axiosImproved/index.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+export default () => {
+  const token = localStorage.getItem('friendAppToken')
+    ? localStorage.getItem('friendAppToken')
+    : 'not signed in';
+
+  return axios.create({
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `${token}`,
+    },
+  });
+};
