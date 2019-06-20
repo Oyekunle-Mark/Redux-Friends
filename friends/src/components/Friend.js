5b48cedd2e5bc1336322bdb844714e518713c429
oyekunle-mark <oyekunlemac@gmail.com>
2019-06-20 18:39:17 +0100

    Styled the friends component

--- friends/src/components/Friend.js
+++ friends/src/components/Friend.js
@@ -1,10 +1,46 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import styled from 'styled-components';
 
 import { deleteFriend } from '../redux/actionCreators';
 
+const StyledFriend = styled.div`
+  min-width: 500px;
+  background: rgb(14, 13, 13);
+  color: rgb(255, 21, 21);
+  margin: 20px 0;
+  padding: 15px;
+  font-family: 'Courgette', cursive;
+
+  h4 {
+    font-size: 25px;
+  }
+
+  h4 span {
+    font-size: 17px;
+  }
+
+  div {
+    margin-top: 10px;
+  }
+
+  div button {
+    margin-top: 10px;
+    height: 25px;
+    background: white;
+    color: rgb(14, 13, 13);
+    border: 1px solid white;
+    font-size: 12px;
+    font-weight: bold;
+  }
+
+  div button:hover {
+    cursor:pointer;
+  }
+`;
+
 const Friend = ({ friend, deleteFriend }) => (
-  <div>
+  <StyledFriend>
     <h4>
       {friend.name}, <span>{friend.age}</span>
     </h4>
@@ -12,7 +48,7 @@ const Friend = ({ friend, deleteFriend }) => (
       <p>{friend.email}</p>
       <button onClick={() => deleteFriend(friend.id)}>Delete Friend</button>
     </div>
-  </div>
+  </StyledFriend>
 );
 
 export default connect(
--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -1,15 +1,22 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
+import styled from 'styled-components';
 
 import Friend from './Friend';
 import { getFriends } from '../redux/actionCreators';
+
+const StyledFriends = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 const Friends = ({ friends, getFriends }) => {
   useEffect(getFriends, []);
 
   const friendList = friends.map(fr => <Friend key={fr.id} friend={fr} />);
 
-  return <div>{friendList}</div>;
+  return <StyledFriends>{friendList}</StyledFriends>;
 };
 
 const mapStateToProps = state => ({
