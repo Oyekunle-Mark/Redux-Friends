c625ead9b1c836a2c81993d7c732bd4bf676f51b
oyekunle-mark <oyekunlemac@gmail.com>
2019-06-20 18:39:53 +0100

    Styled the login form

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,7 +1,37 @@
 import React, { createRef } from 'react';
 import { connect } from 'react-redux';
+import styled from 'styled-components';
 
 import { login } from '../redux/actionCreators';
+
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+
+  margin: 50px;
+
+  input {
+    border: 1px solid rgb(14, 13, 13);
+    width: 300px;
+    height: 23px;
+    padding: 2px;
+    margin: 10px 0;
+  }
+
+  button {
+    margin-top: 10px;
+    height: 26px;
+    background: rgb(255, 21, 21);
+    color: white;
+    border: 1px solid rgb(14, 13, 13);
+    font-size: 12px;
+    font-weight: bold;
+  }
+
+  button:hover {
+    cursor: pointer;
+  }
+`;
 
 const Form = ({ login, history }) => {
   const inputValue = createRef();
@@ -11,15 +41,15 @@ const Form = ({ login, history }) => {
     e.preventDefault();
 
     login(inputValue.current.value, passwordValue.current.value);
-    setTimeout(() => history.push("/"), 1000);
+    setTimeout(() => history.push('/'), 1000);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" ref={inputValue} />
-      <input type="text" ref={passwordValue} />
+    <StyledForm onSubmit={handleSubmit}>
+      <input type="text" placeholder="username" ref={inputValue} />
+      <input type="password" placeholder="password" ref={passwordValue} />
       <button type="submit">Login</button>
-    </form>
+    </StyledForm>
   );
 };
 
